02e11018f6622c8d0103f59367699ebc47456517
Kirito <572745813@qq.com>
2018-10-24 15:21:20 +0800

    Update

---
+++ src/main/java/core/Move.java
@@ -0,0 +1,21 @@
+package core;
+
+import java.io.Serializable;
+
+public class Move implements Serializable {
+    public final int row;
+    public final int col;
+
+    /**
+     * 移动方法
+     * @param row 行
+     * @param col 列
+     */
+    public Move(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+
+
+}
---
+++ src/main/java/players/Player.java
@@ -0,0 +1,8 @@
+package players;
+
+import core.Move;
+
+public abstract class Player {
+
+    public abstract Move getMove();
+}
