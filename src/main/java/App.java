4efb212c7d2537f3fc991440584cf76c6b41c7e8
hsernos <hsernos@163.com>
2018-10-24 15:44:23 +0800

    更改项目结构

---
+++ src/main/java/App.java
@@ -0,0 +1,8 @@
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        // TODO 主界面
+    }
+}
--- src/main/java/players/human/HumanPlayer.java
+++ src/main/java/players/human/HumanPlayer.java
@@ -1,7 +1,13 @@
 package players.human;
 
+import core.Move;
 import players.Player;
 
 public class HumanPlayer extends Player {
 
+    @Override
+    public Move getMove() {
+        // TODO
+        return null;
+    }
 }
