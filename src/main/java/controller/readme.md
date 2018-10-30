fb34d0c027ebebea884ca67de82c3e206c2c10a2
hsernos <hsernos@163.com>
2018-10-30 08:06:03 +0800

    更改项目结构

--- src/main/java/controller/readme.md
+++
@@ -1,2 +0,0 @@
-#Controller类——实现对Fxml文件的控制
-StartController类——开始界面控制
---
+++ src/main/java/entity/Pieces.java
@@ -0,0 +1,8 @@
+package entity;
+
+public class Pieces {
+    private int col;
+    private int row;
+    private int index;
+
+}
