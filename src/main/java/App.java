0a9cbe48283a04157ee02c2fb9aa56a1c6a56366
AbigailCui <1250682372@qq.com>
2018-10-30 09:03:33 +0800

    更改项目结构

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,25 @@
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.JavaFXBuilderFactory;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getClassLoader().getResource("fxml/start.fxml"));
+        fxmlLoader.setBuilderFactory(new JavaFXBuilderFactory());
+        Parent root = fxmlLoader.load();
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 }
